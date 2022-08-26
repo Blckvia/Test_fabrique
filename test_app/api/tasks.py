5036c631f22475fd227fed2967f8ecfdb16c52b5
Alex Vyatkin <76395998+Blckvia@users.noreply.github.com>
2022-08-26 20:45:53 +0500

    Delete tasks.py

--- test_app/api/tasks.py
+++
@@ -1,6 +0,0 @@
-class MissingValueException(Exception):
-    """Создаем свое исключения при отсутствии переменных окружения."""
-
-
-class GetAPIException(Exception):
-    """Создаем свое исключение при сбое запроса к API отправки сообщений."""
